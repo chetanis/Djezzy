150b1a82cec607b03a740a6aa470d2f5cbe203df
chetanis <anis.chetouane@etu.usthb.dz>
2024-03-31 15:19:05 +0100

    crypting the password

--- services/userService.js
+++ services/userService.js
@@ -1,13 +1,16 @@
 const pool = require('../db');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 async function authenticateUser(email, password) {
     try {
         const [rows] = await pool.query('SELECT * FROM users WHERE email = ?', [email]);
         if (rows.length > 0 ) {
-
             const user = { ...rows[0] };
+            // Compare the hashed password with the password provided by the user
+            const passwordMatch = await bcrypt.compare(password, user.password);
 
-            if (password === user.password) {
+            if (passwordMatch) {
                 delete user.password; // Remove the password field from the user object
                 return user; // User found and password matched
             } else {
@@ -22,7 +25,10 @@ async function authenticateUser(email, password) {
 
 async function registerUser(full_name, email, password) {
     try {
-        const [rows] = await pool.query('INSERT INTO users (full_name, email, password) VALUES (?, ?, ?)', [full_name, email, password]);
+        //hashing the password
+        hashedPass = await bcrypt.hash(password, saltRounds);
+
+        const [rows] = await pool.query('INSERT INTO users (full_name, email, password) VALUES (?, ?, ?)', [full_name, email, hashedPass]);
         return rows.insertId;
     } catch (error) {
         console.error('Error in registerUser:', error);
